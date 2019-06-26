a2a48bc4920abc8b6f67aaaf438a20e6853874f1
renr <123444@aliyun.com>
2019-06-26 23:19:19 +0800

    Initial commit

---
+++ src/main/java/com/qf/ooo/ooo/OooApplication.java
@@ -0,0 +1,13 @@
+package com.qf.ooo.ooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qf/ooo/ooo/OooApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qf.ooo.ooo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OooApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
